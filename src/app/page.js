9af24134406ba044f64181e6b17d021b281a6c58
vivekraman111 <vivekraman111@gmail.com>
2023-08-31 15:35:33 +0530

    handle null msg

--- src/app/page.js
+++ src/app/page.js
@@ -29,7 +29,7 @@ export default async function Home() {
           </a>
         </div>
       </div>
-      <p>{msg}</p>
+      <p>{msg || "no message"}</p>
       <div className={styles.center}>
         <Image
           className={styles.logo}
